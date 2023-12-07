69f5277ce602ad7334c6663bfb20d9db4db29b28
Jan ATAC <can.atac@gmail.com>
2023-12-07 17:12:23 +0000

    add some debug logs

--- src/main.rs
+++ src/main.rs
@@ -28,9 +28,10 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr().unwrap());
                 handle_client(stream);
             }
-            Err(e) => { /* connection failed */ }
+            Err(e) => { println!("Error: {}", e); }
         }
     }
 }
