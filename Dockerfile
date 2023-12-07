e50a68e695907054da50e6333a9cbc047e77f352
Jan ATAC <can.atac@gmail.com>
2023-12-07 16:33:11 +0000

    init

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+From rust:1.70
+ARG USER=default_user
+RUN adduser --disabled-password --gecos "" ${USER}
+
+# Utilisation de la variable USER
+RUN echo "Current user is $USER"
+
+VOLUME /data
+
+# Set a different target directory
+# Create the target directory
+
+RUN mkdir -p /app && chown -R ${USER} /app
+
+WORKDIR /app
+USER ${USER}
+
+COPY . .
+
+RUN rustup override set nightly
+RUN cargo version
+RUN cargo install --path .
+
+ENV ROCKET_ADDRESS=0.0.0.0
+EXPOSE 2525
+CMD ["cargo", "run"]
---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+
+    let message = String::from_utf8_lossy(&buffer[..]);
+
+    if message.starts_with("HELO") || message.starts_with("EHLO") {
+        stream.write(b"250 Hello\r\n").unwrap();
+    } else if message.starts_with("MAIL FROM:") {
+        stream.write(b"250 OK\r\n").unwrap();
+    } else if message.starts_with("RCPT TO:") {
+        stream.write(b"250 OK\r\n").unwrap();
+    } else if message.starts_with("DATA") {
+        stream.write(b"354 Start mail input; end with <CRLF>.<CRLF>\r\n").unwrap();
+    } else if message == ".\r\n" {
+        stream.write(b"250 OK\r\n").unwrap();
+    } else {
+        stream.write(b"500 Syntax error, command unrecognized\r\n").unwrap();
+    }
+}
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:2525").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_client(stream);
+            }
+            Err(e) => { /* connection failed */ }
+        }
+    }
+}
