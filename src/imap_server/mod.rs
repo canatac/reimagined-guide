8cc39f36b36d5684565ba1d2418b874e8490694f
jan_atac <jan.atac@ca-indosuez.com>
2024-12-13 13:03:41 +0100

    change client connection to db

--- src/imap_server/mod.rs
+++ src/imap_server/mod.rs
@@ -127,7 +127,19 @@ async fn process_imap_command(command: &[u8], logic: &Arc<Logic>) -> String {
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let client = Arc::new(Client::with_uri_str("mongodb://localhost:27017").await.unwrap());
+    
+    let client_uri = format!(
+        "mongodb+srv://{}:{}@{}/?retryWrites=true&w=majority&appName={}",
+        env::var("MONGODB_USERNAME").expect("MONGODB_USERNAME must be set"),
+        env::var("MONGODB_PASSWORD").expect("MONGODB_PASSWORD must be set"),
+        env::var("MONGODB_CLUSTER_URL").expect("MONGODB_CLUSTER_URL must be set"),
+        env::var("MONGODB_APP_NAME").expect("MONGODB_APP_NAME must be set")
+    );
+    let client = Arc::new(
+        mongodb::Client::with_uri_str(&client_uri)
+            .await?
+    );
+
     let logic = Arc::new(Logic::new(client));
     let server = ImapServer::new(logic);
     let imap_server_address = env::var("IMAP_SERVER").expect("IMAP_SERVER must be set");
