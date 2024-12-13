0c6355fa22ff7738b972b885e11ce95654f69d59
jan_atac <jan.atac@ca-indosuez.com>
2024-12-13 13:36:40 +0100

    change imap_server structure 2

---
+++ src/bin/imap_server.rs
@@ -0,0 +1,32 @@
+use std::sync::Arc;
+use mongodb::Client;
+use std::env;
+use simple_smtp_server::logic::Logic;
+use simple_smtp_server::imap_server::ImapServer;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+    env_logger::init();
+
+    let client_uri = format!(
+        "mongodb+srv://{}:{}@{}/?retryWrites=true&w=majority&appName={}",
+        env::var("MONGODB_USERNAME").expect("MONGODB_USERNAME must be set"),
+        env::var("MONGODB_PASSWORD").expect("MONGODB_PASSWORD must be set"),
+        env::var("MONGODB_CLUSTER_URL").expect("MONGODB_CLUSTER_URL must be set"),
+        env::var("MONGODB_APP_NAME").expect("MONGODB_APP_NAME must be set")
+    );
+    println!("Client URI: {}", client_uri);
+
+    let client = Arc::new(
+        mongodb::Client::with_uri_str(&client_uri)
+            .await.unwrap()
+    );
+
+    let logic = Arc::new(Logic::new(client));
+    let server = ImapServer::new(logic);
+    let imap_server_address = env::var("IMAP_SERVER").expect("IMAP_SERVER must be set");
+    server.run(&imap_server_address).await.unwrap();
+
+    Ok(())
+} 
