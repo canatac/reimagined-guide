aad3ff7e15acee187f88fe63abf20bfcee2717ba
jan_atac <jan.atac@ca-indosuez.com>
2024-12-13 13:35:46 +0100

    change imap_server structure

--- src/imap_server/mod.rs
+++ src/imap_server/mod.rs
@@ -124,28 +124,3 @@ async fn process_imap_command(command: &[u8], logic: &Arc<Logic>) -> String {
         _ => "BAD Command not recognized\r\n".to_string(),
     }
 }
-
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
-    
-    let client_uri = format!(
-        "mongodb+srv://{}:{}@{}/?retryWrites=true&w=majority&appName={}",
-        env::var("MONGODB_USERNAME").expect("MONGODB_USERNAME must be set"),
-        env::var("MONGODB_PASSWORD").expect("MONGODB_PASSWORD must be set"),
-        env::var("MONGODB_CLUSTER_URL").expect("MONGODB_CLUSTER_URL must be set"),
-        env::var("MONGODB_APP_NAME").expect("MONGODB_APP_NAME must be set")
-    );
-    println!("Client URI: {}", client_uri);
-
-    let client = Arc::new(
-        mongodb::Client::with_uri_str(&client_uri)
-                .await.unwrap()
-    );
-
-    let logic = Arc::new(Logic::new(client));
-    let server = ImapServer::new(logic);
-    let imap_server_address = env::var("IMAP_SERVER").expect("IMAP_SERVER must be set");
-    server.run(&imap_server_address).await.unwrap();
-
-    Ok(())
-} 
