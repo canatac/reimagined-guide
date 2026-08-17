3da607f096056b6f59a45fdc304d97f98e2692f4
Jan ATAC <can.atac@gmail.com>
2026-08-17 07:35:30 +0000

    restore(deliverability): re-add submodule files perdus par merge

--- src/bin/email_api_dir/admin_ops/diag_handlers/deliverability/mod.rs
+++ src/bin/email_api_dir/admin_ops/diag_handlers/deliverability/mod.rs
@@ -3,6 +3,6 @@ pub mod diagnostics;
 pub mod procedure;
 pub mod procedure_update;
 
-pub use diagnostics::api_admin_deliverability_diagnostics;
-pub use procedure::api_admin_deliverability_procedure;
-pub use procedure_update::api_admin_deliverability_procedure_update;
+pub(crate) use diagnostics::api_admin_deliverability_diagnostics;
+pub(crate) use procedure::api_admin_deliverability_procedure;
+pub(crate) use procedure_update::api_admin_deliverability_procedure_update;
